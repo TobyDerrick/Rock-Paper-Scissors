7a05d165408a8d9ff0ebfbc4514d9eecfbe4927d
TobyDerrick <tobyderrick76@gmail.com>
2024-07-02 11:12:04 +0100

    [added] sounds for winning and losing

--- SCENES/Battle/battle.gd
+++ SCENES/Battle/battle.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var enemy_stats: CharacterStats
 @export var best_of: float =  3
 @export var music: AudioStream
+@export var battle_win_sound: AudioStream
+@export var battle_loss_sound: AudioStream
 
 @onready var battle_ui: BattleUI = $BattleUI as BattleUI
 @onready var player_handler: PlayerHandler = $PlayerHandler as PlayerHandler
@@ -22,6 +24,7 @@ func _ready():
 	Events.player_turn_ended.connect(enemy_handler.start_turn)
 	Events.finished_comparing_stacks.connect(player_handler.start_turn)
 	Events.finished_comparing_stacks.connect(_on_finished_comparing_stacks)
+	Events.battle_completed.connect(_battle_completed)
 	
 func start_battle(stats: CharacterStats, enemy_char_stats: CharacterStats) -> void:
 	MusicPlayer.play(music, true)
@@ -37,3 +40,11 @@ func _on_finished_comparing_stacks(round_result: GlobalEnums.round_result):
 	
 	if round_wins >= round(best_of / 2) or round_losses >= round(best_of / 2):
 		Events.battle_completed.emit(round_result)
+
+func _battle_completed(round_result: GlobalEnums.round_result):
+	if round_result == GlobalEnums.round_result.WIN:
+		SfxPlayer.play(battle_win_sound, true)
+	
+	else:
+		SfxPlayer.play(battle_loss_sound, true)
+	
